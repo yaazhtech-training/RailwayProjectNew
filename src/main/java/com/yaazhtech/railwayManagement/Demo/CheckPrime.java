5e984b7711f0b8f3c11c55d33e93ac07866cdf8d
jeyamaniyaazhtech <surjeyam@outlook.com>
2024-05-07 09:19:53 +0400

    PrimeNo and Largest No
    Find out Prime No
    Find out Largest No in Array

---
+++ src/main/java/com/yaazhtech/railwayManagement/Demo/CheckPrime.java
@@ -0,0 +1,31 @@
+package com.yaazhtech.railwayManagement.Demo;
+
+public class CheckPrime {
+
+    static boolean isPrime(int n)
+    {
+        // Corner case
+        if (n <= 1)
+            return false;
+
+        // Check from 2 to n-1
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+
+    // Driver Program
+    public static void main(String args[])
+    {
+        if (isPrime(11))
+            System.out.println(" true");
+        else
+            System.out.println(" false");
+        if (isPrime(15))
+            System.out.println(" true");
+        else
+            System.out.println(" false");
+    }
+}
